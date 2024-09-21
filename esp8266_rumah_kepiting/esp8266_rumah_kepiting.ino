8b0ee59a9b1cf43959b12ca081a5d832bcb783f0
nathan <nathanaelgilberth353@gmail.com>
2024-09-21 16:25:18 +0700

    Initial Commit

---
+++ esp8266_rumah_kepiting/esp8266_rumah_kepiting.ino
@@ -0,0 +1,39 @@
+#define BLYNK_TEMPLATE_ID "TEMPLATE_ID"
+#define BLYNK_TEMPLATE_NAME "TEMPLATE_NAME"
+#define BLYNK_AUTH_TOKEN "AUTH_TOKEN"
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// Define Temperature Sensors
+#define ONE_WIRE_BUS 4
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+// Connect To Wifi
+char ssid[] = "WIFI_SSID";
+char pass[] = "WIFI_PASSWORD";
+
+// Define Blynk
+BlynkTimer timer;
+
+void sendTempDataTimer() {
+  sensors.requestTemperatures(); 
+  double temperatureC = sensors.getTempCByIndex(0); // Get temperature data from sensors
+  Blynk.virtualWrite(V0, temperatureC);
+}
+
+void setup(void) {
+  Serial.begin(115200);
+  sensors.begin();
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  // Send data to BLYNK every 1s (1000ms)
+  timer.setInterval(1000L, sendTempDataTimer);
+}
+
+void loop(void) {
+  Blynk.run();
+  timer.run();
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Smart Rumah Kepiting Project
+Project about crab house monitoring system using ESP8266 and BLYNK.
